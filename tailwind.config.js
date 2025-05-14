aeaece154175041761b344d5591b1260a84d5448
Bedo <bedonassef71@gmail.com>
2025-05-15 00:06:08 +0300

    Login form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 export default {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+        extend: {
+            colors: {
+                'black-dark': '#00000050',
+                'dull-white': '#FFFFFFB3',
+                'white-light': '#FFFFFF30',
+                'white-medium': '#FFFFFF40',
+                'neon-blue': '#2FB8FF',
+            },
+        },
     },
     plugins: [],
-  }
+}
